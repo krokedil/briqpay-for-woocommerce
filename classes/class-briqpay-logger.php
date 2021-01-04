39d7507b908595debfbf9c068edcc77e597f35ac
Miodrag Stojanovic <mio.sto@yandex.com>
2021-01-04 14:09:31 +0100

    add logger class

---
+++ classes/class-briqpay-logger.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * Logging class file.
+ *
+ * @package Briqpay_For_WooCommerce/Classes
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Briqpay_Logger class.
+ */
+class Briqpay_Logger {
+	/**
+	 * Log message string
+	 *
+	 * @var $log
+	 */
+	private static $log;
+
+	/**
+	 * Logs an event.
+	 *
+	 * @param string $data The data string.
+	 */
+	public static function log( $data ) {
+		$settings = get_option( 'woocommerce_briqpay_settings' );
+		if ( 'yes' === $settings['logging'] ) {
+			$message = self::format_data( $data );
+			if ( empty( self::$log ) ) {
+				self::$log = new WC_Logger();
+			}
+			self::$log->add( 'briqpay-for-woocommerce', wp_json_encode( $message ) );
+		}
+	}
+
+	/**
+	 * Formats the log data to prevent json error.
+	 *
+	 * @param string $data Json string of data.
+	 * @return array
+	 */
+	public static function format_data( $data ) {
+		$data = json_decode( $data, true );
+		if ( isset( $data['request']['body'] ) ) {
+			$data['request']['body'] = json_decode( $data['request']['body'], true );
+		}
+		return $data;
+	}
+
+	/**
+	 * Formats the log data to be logged.
+	 *
+	 * @param string $briqpay_order_id The Briqpay order id.
+	 * @param string $method The method.
+	 * @param string $title The title for the log.
+	 * @param array  $request_args The request args.
+	 * @param array  $response The response.
+	 * @param string $code The status code.
+	 *
+	 * @return array
+	 */
+	public static function format_log( $briqpay_order_id, $method, $title, $request_args, $response, $code ) {
+		// Unset the snippet to prevent issues in the response.
+		if ( isset( $response['html_snippet'] ) ) {
+			unset( $response['html_snippet'] );
+		}
+		// Unset the snippet to prevent issues in the request body.
+		if ( isset( $request_args['body'] ) ) {
+			$request_body = json_decode( $request_args['body'], true );
+			if ( isset( $request_body['html_snippet'] ) && $request_body['snippet'] ) {
+				unset( $request_body['html_snippet'] );
+				$request_args['body'] = wp_json_encode( $request_body );
+			}
+		}
+		return array(
+			'id'             => $briqpay_order_id,
+			'type'           => $method,
+			'title'          => $title,
+			'request'        => $request_args,
+			'response'       => array(
+				'body' => $response,
+				'code' => $code,
+			),
+			'timestamp'      => date( 'Y-m-d H:i:s' ), // phpcs:ignore WordPress.DateTime.RestrictedFunctions -- Date is not used for display.
+			'stack'          => self::get_stack(),
+			'plugin_version' => BRIQPAY_WC_PLUGIN_VERSION,
+		);
+	}
+
+	/**
+	 * Gets the stack for the request.
+	 *
+	 * @return array
+	 */
+	public static function get_stack() {
+		$debug_data = debug_backtrace(); // phpcs:ignore WordPress.PHP.DevelopmentFunctions -- Data is not used for display.
+		$stack      = array();
+		foreach ( $debug_data as $data ) {
+			$extra_data = '';
+			if ( ! in_array( $data['function'], array( 'get_stack', 'format_log' ), true ) ) {
+				if ( in_array( $data['function'], array( 'do_action', 'apply_filters' ), true ) ) {
+					if ( isset( $data['object'] ) ) {
+						$priority   = $data['object']->current_priority();
+						$name       = key( $data['object']->current() );
+						$extra_data = $name . ' : ' . $priority;
+					}
+				}
+			}
+			$stack[] = $data['function'] . $extra_data;
+		}
+		return $stack;
+	}
+
+}
