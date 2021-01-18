1dc315dfbf2584014fe485015a0c87e66a6d4da5
Miodrag Stojanovic <mio.sto@yandex.com>
2021-01-18 12:20:08 +0100

    add a request class for authorization

---
+++ classes/requests/get/class-briqpay-request-auth.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Request class for making auth request
+ *
+ * @package Briqpay_For_WooCommerce/Classes/Requests/Get
+ */
+
+/**
+ * Class Briqpay_Request_Auth
+ */
+class Briqpay_Request_Auth extends Briqpay_Request_Get {
+
+	/**
+	 * Empty authorization header for authentication POSTs
+	 * Returns nothing so there is no Authorization header. The actual authentication is done in the request body.
+	 *
+	 * @return void
+	 */
+	public function calculate_auth() { }
+
+	/**
+	 * Obtain an bearer token for Briqpay based on the configured credentials
+	 *
+	 * @return string|object Either a fresh Briqpay token, or a null value if the request failed.
+	 */
+	public function obtain_token() {
+		$response = $this->request();
+		if ( is_wp_error( $response ) ) {
+			// This block is essentially useless, but I'm leaving it in in case the error needs to be modified later.
+			return $response; // For now, just pass the WP error up the chain.
+		}
+		return $response;
+	}
+
+	/**
+	 *  Get the request url.
+	 *
+	 * @return string|void
+	 */
+	protected function get_request_url() {
+		return $this->get_api_url_base() . 'auth';
+	}
+
+	/**
+	 *  Get the request args.
+	 *
+	 * @return array
+	 */
+	protected function get_request_args() {
+		return $this->arguments;
+	}
+}
