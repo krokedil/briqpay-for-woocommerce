be00f7c8f9472df156145c10cbba8afc0008ac8e
Miodrag Stojanovic <mio.sto@yandex.com>
2021-01-20 09:27:11 +0100

    add an ajax function for logging

--- classes/class-briqpay-ajax.php
+++ classes/class-briqpay-ajax.php
@@ -26,6 +26,7 @@ class Briqpay_Ajax extends WC_AJAX {
 	public static function add_ajax_events() {
 		$ajax_events = array(
 			'briqpay_get_order' => true,
+			'briqpay_wc_log_js' => true,
 		);
 		foreach ( $ajax_events as $ajax_event => $nopriv ) {
 			add_action( 'wp_ajax_woocommerce_' . $ajax_event, array( __CLASS__, $ajax_event ) );
@@ -56,6 +57,25 @@ class Briqpay_Ajax extends WC_AJAX {
 				'shipping_address' => $briqpay_order['shippingaddress'],
 			)
 		);
+		wp_die();
+	}
+
+	/**
+	 * Logs messages from the JavaScript to the server log.
+	 *
+	 * @return void
+	 */
+	public static function briqpay_wc_log_js() {
+		$nonce = isset( $_POST['nonce'] ) ? sanitize_key( $_POST['nonce'] ) : '';
+		if ( ! wp_verify_nonce( $nonce, 'briqpay_wc_log_js' ) ) {
+			wp_send_json_error( 'bad_nonce' );
+			exit;
+		}
+		$posted_message     = isset( $_POST['message'] ) ? sanitize_text_field( wp_unslash( $_POST['message'] ) ) : '';
+		$briqpay_session_id = WC()->session->get( 'briqpay_session_id' );
+		$message            = "Frontend JS $briqpay_session_id: $posted_message";
+		Briqpay_Logger::log( $message );
+		wp_send_json_success();
 		wp_die();
 	}
 
