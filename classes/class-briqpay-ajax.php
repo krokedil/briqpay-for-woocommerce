d46cedac59bd8dfeb1c1262704b510b2642f34e3
Miodrag Stojanovic <mio.sto@yandex.com>
2021-02-02 09:29:39 +0100

    Add ajax function for updating the order

--- classes/class-briqpay-ajax.php
+++ classes/class-briqpay-ajax.php
@@ -25,8 +25,9 @@ class Briqpay_Ajax extends WC_AJAX {
 	 */
 	public static function add_ajax_events() {
 		$ajax_events = array(
-			'briqpay_get_order' => true,
-			'briqpay_wc_log_js' => true,
+			'briqpay_get_order'          => true,
+			'briqpay_wc_log_js'          => true,
+			'briqpay_wc_update_checkout' => true,
 		);
 		foreach ( $ajax_events as $ajax_event => $nopriv ) {
 			add_action( 'wp_ajax_woocommerce_' . $ajax_event, array( __CLASS__, $ajax_event ) );
@@ -40,7 +41,7 @@ class Briqpay_Ajax extends WC_AJAX {
 
 
 	/**
-	 *
+	 * Gets the Briqpay order.
 	 */
 	public static function briqpay_get_order() {
 		$briqpay_order = BRIQPAY()->api->get_briqpay_order(
@@ -77,5 +78,62 @@ class Briqpay_Ajax extends WC_AJAX {
 		wp_die();
 	}
 
+	/**
+	 *  Update the Briqpay order.
+	 */
+	public static function briqpay_wc_update_checkout() {
+
+		if ( ! WC()->cart->needs_payment() ) {
+			wp_send_json_success(
+				array(
+					'refreshZeroAmount' => 'refreshZeroAmount',
+				)
+			);
+			wp_die();
+		}
+		$action = 'briqpay_wc_update_checkout';
+		if ( ! isset( $_POST['nonce'] ) || ! wp_verify_nonce( sanitize_key( $_POST['nonce'] ), $action ) ) {
+			wp_send_json_error( 'bad_nonce' );
+			exit;
+		}
+		WC()->cart->calculate_fees();
+		WC()->cart->calculate_shipping();
+		WC()->cart->calculate_totals();
+
+		// Update the briqpay order.
+		$response = BRIQPAY()->api->update_briqpay_order(
+			array(
+				'session_id' => WC()->session->get( 'briqpay_session_id' ),
+			)
+		);
+		if ( is_wp_error( $response ) ) {
+			// If error return error message.
+			$code           = $response->get_error_code();
+			$message        = $response->get_error_message();
+			$text           = __( 'Briqpay API Error: ', 'briqpay-for-woocommerce' ) . '%s %s';
+			$formatted_text = sprintf( $text, $code, $message );
+			wp_send_json_error( $formatted_text );
+			wp_die();
+		}
+
+		if ( false === $response ) {
+			$changed = false;
+		} else {
+			$changed = true;
+		}
+
+		$address = array(
+			'billing_address'  => isset( $response['billingaddress'] ) ? $response['billingaddress'] : array(),
+			'shipping_address' => isset( $response['shippingaddress'] ) ? $response['shippingaddress'] : array(),
+		);
+		wp_send_json_success(
+			array(
+				'address' => $address,
+				'changed' => $changed,
+			)
+		);
+		wp_die();
+
+	}
 }
 Briqpay_Ajax::init();
