17dc69a6a9e3d12f171e0c75bdfb35cff7182a07
Miodrag Stojanovic <mio.sto@yandex.com>
2021-02-02 09:38:08 +0100

    update the request arguments

--- classes/requests/post/class-briqpay-request-create.php
+++ classes/requests/post/class-briqpay-request-create.php
@@ -18,14 +18,10 @@ class Briqpay_Request_Create extends Briqpay_Request_Post {
 	protected function get_body() {
 		$amount = intval( round( WC()->cart->get_total( 'calculations' ) * 100 ) );
 		return array(
-			'currency'     => 'SEK',
-			'locale'       => 'sv-se',
+			'currency'     => get_woocommerce_currency(),
+			'locale'       => str_replace( '_', '-', strtolower( get_locale() ) ),
 			'country'      => WC()->customer->get_billing_country(),
-			'merchanturls' => array(
-				'terms'         => get_permalink( wc_get_page_id( 'terms' ) ),
-				'notifications' => home_url( '/wc-api/BRIQPAY_WC_NOTIFICATION' ),
-				'redirecturl'   => home_url( '?briqpay-success' ),
-			),
+			'merchanturls' => Briqpay_Helper_MerchantUrls::get_urls(),
 			'cart'         => Briqpay_Helper_Cart::get_cart_items(),
 			'amount'       => $amount,
 		// 'billingaddress'  => Briqpay_Helper_Customer::get_billing_data(),
