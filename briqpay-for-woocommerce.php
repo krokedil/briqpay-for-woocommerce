8185c55814db8c257b07a042651eded0c1b7d721
Niklas Högefjord <niklas@krokedil.se>
2021-07-12 10:19:45 +0200

    Version update (1.2.1)

--- briqpay-for-woocommerce.php
+++ briqpay-for-woocommerce.php
@@ -5,12 +5,12 @@
  * Description: Briqpay for WooCommerce.
  * Author: Krokedil
  * Author URI: https://krokedil.com/
- * Version: 1.2.0
+ * Version: 1.2.1
  * Text Domain: briqpay-for-woocommerce
  * Domain Path: /languages
  *
  * WC requires at least: 4.0.0
- * WC tested up to: 5.3.0
+ * WC tested up to: 5.4.1
  *
  * Copyright (c) 2021 Krokedil
  *
@@ -38,7 +38,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 define( 'BRIQPAY_WC_MAIN_FILE', __FILE__ );
 define( 'BRIQPAY_WC_PLUGIN_PATH', untrailingslashit( plugin_dir_path( __FILE__ ) ) );
 define( 'BRIQPAY_WC_PLUGIN_URL', untrailingslashit( plugin_dir_url( __FILE__ ) ) );
-define( 'BRIQPAY_WC_PLUGIN_VERSION', '1.2.0' );
+define( 'BRIQPAY_WC_PLUGIN_VERSION', '1.2.1' );
 
 if ( ! class_exists( 'Briqpay_For_WooCommerce' ) ) {
 	/**
