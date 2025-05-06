9c6e1410ca7e491b4e86b88c2325f6f60863063d
anyakro <anya@krokedil.se>
2025-05-06 09:04:27 +0200

    Version 1.8.3
    * Fix - Resolved an issue where the shipping phone number was not being set correctly.

--- briqpay-for-woocommerce.php
+++ briqpay-for-woocommerce.php
@@ -5,12 +5,12 @@
  * Description: Briqpay for WooCommerce.
  * Author: Krokedil
  * Author URI: https://krokedil.com/
- * Version: 1.8.2
+ * Version: 1.8.3
  * Text Domain: briqpay-for-woocommerce
  * Domain Path: /languages
  *
  * WC requires at least: 4.0.0
- * WC tested up to: 9.4.1
+ * WC tested up to: 9.8.3
  *
  * Copyright (c) 2021-2025 Krokedil
  *
@@ -38,7 +38,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 define( 'BRIQPAY_WC_MAIN_FILE', __FILE__ );
 define( 'BRIQPAY_WC_PLUGIN_PATH', untrailingslashit( plugin_dir_path( __FILE__ ) ) );
 define( 'BRIQPAY_WC_PLUGIN_URL', untrailingslashit( plugin_dir_url( __FILE__ ) ) );
-define( 'BRIQPAY_WC_PLUGIN_VERSION', '1.8.2' );
+define( 'BRIQPAY_WC_PLUGIN_VERSION', '1.8.3' );
 
 if ( ! class_exists( 'Briqpay_For_WooCommerce' ) ) {
 	/**
